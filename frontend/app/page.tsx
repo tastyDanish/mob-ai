54e1a5f05a7ae284d0fef9115a8370ebddcee430
Peter Lansdaal <peter@gmail.com>
2023-07-31 12:51:07 -0700

    no box in the center

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,16 +1,17 @@
 "use client";
-import { useEffect, useState } from "react";
+import { LegacyRef, useEffect, useRef, useState } from "react";
 import styles from "../styles/page.module.css";
 import { addWord } from "@/api";
 import { useWords, WordsContextType } from "../context/WordsContext";
 import WordList from "@/components/WordList";
-import { useAnimate } from "framer-motion";
+import { motion, useAnimate } from "framer-motion";
 
 const Home = () => {
   const { topWords, bottomWords }: WordsContextType = useWords();
   const [text, setText] = useState("");
   const [positive, setPositive] = useState(true);
   const [scope, animate] = useAnimate();
+  const inputRef = useRef<HTMLInputElement | null>(null);
 
   const submit = () => {
     addWord(text, positive);
@@ -24,7 +25,10 @@ const Home = () => {
   };
 
   useEffect(() => {
-    animate(scope.current, { x: positive ? -200 : 200 });
+    animate(scope.current, {
+      x: positive ? -100 : 100,
+    });
+    if (inputRef.current) inputRef.current.focus();
   }, [positive]);
 
   return (
@@ -37,7 +41,7 @@ const Home = () => {
             isPositive={true}
           />
         </div>
-        <div style={{ height: 400, width: 400, background: "grey" }} />
+        <div style={{ height: 200, width: 0, background: "grey" }} />
         <div onClick={() => setPositive(false)}>
           <h2>Bottom Words</h2>
           <WordList
@@ -47,10 +51,13 @@ const Home = () => {
         </div>
       </div>
 
-      <div
+      <motion.div
         ref={scope}
-        className={styles.submitContainer}>
+        className={`${styles.submitContainer} ${
+          positive ? styles.create : styles.destroy
+        }`}>
         <input
+          ref={inputRef}
           value={text}
           onKeyUp={handleKeyUp}
           onChange={(e) => setText(e.target.value)}></input>
@@ -59,7 +66,7 @@ const Home = () => {
           onClick={submit}>
           submit word
         </button>
-      </div>
+      </motion.div>
     </main>
   );
 };
